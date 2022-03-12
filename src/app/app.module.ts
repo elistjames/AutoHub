c439d6bec9daea150b5b0867ed4ba369336c663b
elistjames <78109485+elistjames@users.noreply.github.com>
2022-03-12 16:05:22 -0500

    Set Up Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
