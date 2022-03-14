25cf68302945f5f2f5aa1c279c332fdfdd37accb
graydonbenson <graydonbenson@gmail.com>
2022-03-13 21:25:57 -0600

    45 min mark done

--- src/main.py
+++ src/main.py
@@ -15,14 +15,24 @@ def abort_if_carID_invalid (plateNo):
         if plateNo not in cars:
                 abort(404, message = "Car id not valid")
 
+def abort_if_car_exists(plateNo):
+        if plateNo in cars:
+                abort(409, message = "Car id already exists")
+
 class CAR(Resource):
         def get(self, plateNo):
                 abort_if_carID_invalid(plateNo)
                 return cars[plateNo]
         def put(self, plateNo):
+                abort_if_car_exists(plateNo)
                 args = car_put_args.parse_args()
                 cars[plateNo] = args
                 return cars[plateNo], 201
+
+        def delete(self, plateNo):
+                abort_if_carID_invalid (plateNo)
+                del cars[plateNo]
+                return '', 204
 
 api.add_resource(CAR, "/car/<int:plateNo>")
 
--- src/test.py
+++ src/test.py
@@ -3,7 +3,25 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "car/1", {"plateNum" : "BD 90177", "topSpeed" : 100, "weight" : 4000})
+response = requests.put(BASE + "car/3", {"plateNum" : "BD 90177", "topSpeed" : 100, "weight" : 4000})
+response = requests.put(BASE + "car/1", {"plateNum" : "BD Test", "topSpeed" : 100, "weight" : 400120})
+response = requests.put(BASE + "car/2", {"plateNum" : "BD For I", "topSpeed" : 100, "weight" : 1221})
 input()
 response = requests.get(BASE + "car/1")
 print(response.json())
+input()
+response = requests.get(BASE + "car/2")
+print(response.json())
+input()
+response = requests.get(BASE + "car/3")
+print(response.json())
+input()
+response = requests.get(BASE + "car/4")
+print(response.json())
+input()
+response = requests.delete(BASE + "car/1")
+print(response)
+input()
+response = requests.delete(BASE + "car/1")
+print(response)
+
