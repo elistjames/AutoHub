1542cfe3b86a1ff494c13377581aa3c24442bc4c
graydonbenson <graydonbenson@gmail.com>
2022-03-14 20:30:58 -0600

    Get requests are working

--- src/main.py
+++ src/main.py
@@ -1,40 +1,53 @@
+from dataclasses import field
 from flask import Flask, request;
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class CAR(db.Model):
+        plateNum = db.Column(db.String,primary_key = True)
+        topSpeed = db.Column(db.Integer, nullable = False)
+        weight = db.Column(db.Integer, nullable = False)
+
+        def __repr__(self):
+                return f"Car(plateNum = {plateNum}, topSpeed = {topSpeed}, weight = {weight})"
+
 
 car_put_args = reqparse.RequestParser()
 car_put_args.add_argument ("plateNum", type = str, help = "plateNum needs to be a string", required=True)
 car_put_args.add_argument ("topSpeed", type = int, help = "topSpeed is an int", required=True)
 car_put_args.add_argument ("weight", type = int, help = "weight is an int", required=True)
 
-cars = {}
+resource_fields = {
+        'plateNum' : fields.String,
+        'topSpeed' : fields.Integer,
+        'weight' : fields.Integer
+}
 
-def abort_if_carID_invalid (plateNo):
-        if plateNo not in cars:
-                abort(404, message = "Car id not valid")
 
-def abort_if_car_exists(plateNo):
-        if plateNo in cars:
-                abort(409, message = "Car id already exists")
-
-class CAR(Resource):
+class CARS(Resource):
+        @marshal_with(resource_fields)
         def get(self, plateNo):
-                abort_if_carID_invalid(plateNo)
-                return cars[plateNo]
+                result = CAR.query.filter_by(plateNum = plateNo).first()
+                return result
+
+        @marshal_with(resource_fields)
         def put(self, plateNo):
-                abort_if_car_exists(plateNo)
                 args = car_put_args.parse_args()
-                cars[plateNo] = args
-                return cars[plateNo], 201
+                car = CAR(plateNum = plateNo, topSpeed = args['plateNum'] , weight = args['weight'])
+                db.session.add(car)
+                db.session.commit()
+                return car, 201
 
         def delete(self, plateNo):
-                abort_if_carID_invalid (plateNo)
                 del cars[plateNo]
                 return '', 204
 
-api.add_resource(CAR, "/car/<int:plateNo>")
+api.add_resource(CARS, "/car/<string:plateNo>")
 
 if __name__ == "__main__":
         app.run(debug=True)
--- src/test.py
+++ src/test.py
@@ -3,25 +3,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "car/3", {"plateNum" : "BD 90177", "topSpeed" : 100, "weight" : 4000})
-response = requests.put(BASE + "car/1", {"plateNum" : "BD Test", "topSpeed" : 100, "weight" : 400120})
-response = requests.put(BASE + "car/2", {"plateNum" : "BD For I", "topSpeed" : 100, "weight" : 1221})
-input()
-response = requests.get(BASE + "car/1")
+##response = requests.put(BASE + "car/BD90177", {"topSpeed" : 100, "weight" : 4000})
+response = requests.get(BASE + "car/BD 90177")
 print(response.json())
-input()
-response = requests.get(BASE + "car/2")
-print(response.json())
-input()
-response = requests.get(BASE + "car/3")
-print(response.json())
-input()
-response = requests.get(BASE + "car/4")
-print(response.json())
-input()
-response = requests.delete(BASE + "car/1")
-print(response)
-input()
-response = requests.delete(BASE + "car/1")
-print(response)
-
