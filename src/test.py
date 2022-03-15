123fa74dd0ec9552ee171ccf157b2e7af4f8f4b3
elistjames <ehstjames@gmail.com>
2022-03-15 13:15:26 -0600

    idk

--- src/test.py
+++ src/test.py
@@ -3,9 +3,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "car/BD 90177", {"topSpeed" : 1000, "weight" : 3000})
+response = requests.put(BASE + "car/BD90177", {"topSpeed" : 1000, "weight" : 3000})
 input()
-response = requests.get(BASE + "car/BD 90177")
+response = requests.get(BASE + "car/BD90177")
 print(response.json())
 input()
 response = requests.get(BASE + "car/BD901111")
